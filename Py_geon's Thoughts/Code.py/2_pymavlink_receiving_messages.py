33f1f00ff9793edf96b67fb0e6257670034bbc39
BUK <132343254+git-blame-BUK@users.noreply.github.com>
2023-06-08 15:33:33 +0200

    Update 2_pymavlink_receiving_messages.py

--- Py_geon's Thoughts/Code.py/2_pymavlink_receiving_messages.py
+++ Py_geon's Thoughts/Code.py/2_pymavlink_receiving_messages.py
@@ -1,1 +1,16 @@
+########################## Py_geon #####################
+## Author: BUK
+## listen for mavlink Heartbeats
+from pymavlink import mavutil
 
+# Start a conncection listening on corresponding serial interface 
+# Baudrate(57600) can vary depending on your connected Serial flightcontroller Port (might be e.g. 115 200)
+the_connection = mavutil.mavlink_connection("/dev/ttyTHS1", 57600)
+
+# wait for Heartbeat( sets SystemID and ComponentID)
+the_connection.wait_heartbeat()
+  print(f"Heartbeat from system (SystemID: {the_connection.target_system}, ComponentID: {the_conenction.target_component})")
+
+# receive mavlink Messages(e.g. Attitude)
+while 1:
+  msg = the_connection.recv_match(type="ATTITUDE", blocking=True)
